e11d602337eb81746a210e7d3c149861f2dd595e
roynaldi19 <roynaldi19@gmail.com>
2023-04-28 19:46:22 +0700

    add radius

--- app/src/main/java/com/roynaldi19/dc4_06geofence/MapsActivity.kt
+++ app/src/main/java/com/roynaldi19/dc4_06geofence/MapsActivity.kt
@@ -1,16 +1,25 @@
 package com.roynaldi19.dc4_06geofence
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CircleOptions
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.roynaldi19.dc4_06geofence.databinding.ActivityMapsBinding
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
-    private lateinit var mMap: GoogleMap
+    private lateinit var map: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+
+    private val centerLat = 0.42435
+    private val centerLong = 101.43974
+    private val geofenceRadius = 400.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,14 +27,21 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
+        map = googleMap
+
+        map.uiSettings.isZoomControlsEnabled = true
+
+        val kiddHome = LatLng(centerLat, centerLong)
+        map.addMarker(MarkerOptions().position(kiddHome).title("Kidd Home"))
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(kiddHome, 15f))
+
+        map.addCircle(CircleOptions().center(kiddHome).radius(geofenceRadius).fillColor(0x22FF0000).strokeColor(Color.RED).strokeWidth(3f))
 
 
     }
