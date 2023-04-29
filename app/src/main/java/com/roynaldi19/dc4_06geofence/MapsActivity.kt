ade7e8ad856a606e5cae5801917e62e1d07b8a9b
roynaldi19 <roynaldi19@gmail.com>
2023-04-29 08:05:37 +0700

    add notification

--- app/src/main/java/com/roynaldi19/dc4_06geofence/MapsActivity.kt
+++ app/src/main/java/com/roynaldi19/dc4_06geofence/MapsActivity.kt
@@ -3,13 +3,20 @@ package com.roynaldi19.dc4_06geofence
 import android.Manifest
 import android.annotation.SuppressLint
 import android.annotation.TargetApi
+import android.app.PendingIntent
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import com.google.android.gms.location.Geofence
+import com.google.android.gms.location.GeofencingClient
+import com.google.android.gms.location.GeofencingRequest
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -28,14 +35,15 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private val centerLong = 101.43974
     private val geofenceRadius = 400.0
 
+    private lateinit var geoFencingClient: GeofencingClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
 
@@ -54,7 +62,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         )
 
         getMyLocation()
-
+        addGeofence()
 
     }
 
@@ -84,20 +92,18 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private fun checkPermission(permission: String): Boolean {
         return ContextCompat.checkSelfPermission(
-            this,
-            permission
+            this, permission
         ) == PackageManager.PERMISSION_GRANTED
     }
 
     @TargetApi(Build.VERSION_CODES.Q)
     private fun checkForeGroundAndBackgroundLocationPermission(): Boolean {
         val foregroundLocationApproved = checkPermission(Manifest.permission.ACCESS_FINE_LOCATION)
-        val backgroundPermissionApproved =
-            if (runningQOrLater) {
-                checkPermission(Manifest.permission.ACCESS_BACKGROUND_LOCATION)
-            } else {
-                true
-            }
+        val backgroundPermissionApproved = if (runningQOrLater) {
+            checkPermission(Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+        } else {
+            true
+        }
         return foregroundLocationApproved && backgroundPermissionApproved
     }
 
@@ -108,5 +114,47 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         } else {
             requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
         }
+    }
+
+    private val geofencePendingIntent: PendingIntent by lazy {
+        val intent = Intent(this, GeofenceBroadcastReceiver::class.java)
+        intent.action = GeofenceBroadcastReceiver.ACTION_GEOFENCE_EVENT
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_MUTABLE)
+        } else {
+            PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun addGeofence() {
+        geoFencingClient = LocationServices.getGeofencingClient(this)
+
+        val geoFence = Geofence.Builder().setRequestId("Rumah").setCircularRegion(
+            centerLat, centerLong, geofenceRadius.toFloat()
+        ).setExpirationDuration(Geofence.NEVER_EXPIRE)
+            .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_DWELL or Geofence.GEOFENCE_TRANSITION_ENTER)
+            .setLoiteringDelay(5000).build()
+
+        val geofencingRequest = GeofencingRequest.Builder()
+            .setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER)
+            .addGeofence(geoFence).build()
+
+        geoFencingClient.removeGeofences(geofencePendingIntent).run {
+            addOnCompleteListener {
+                geoFencingClient.addGeofences(geofencingRequest, geofencePendingIntent).run {
+                    addOnSuccessListener {
+                        showToast("Geofencing added")
+                    }
+                    addOnFailureListener {
+                        showToast("Geofencing not added : ${it.message}")
+                    }
+                }
+            }
+        }
+    }
+
+    private fun showToast(text: String) {
+        Toast.makeText(this@MapsActivity, text, Toast.LENGTH_SHORT).show()
     }
 }
